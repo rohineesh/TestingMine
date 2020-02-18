57b43b24c6cc0e3a1ad80f6b8cd8eff6c1e0175e
Rohineesh Chopra <rohineeshchopra@Admins-MacBook-Pro.local>
2020-02-18 16:08:53 +0530

    committing changes

--- handlebars/app.js
+++ handlebars/app.js
@@ -1,3 +1,28 @@
 const handlebars = require('handlebars')
-const template = handlebars.compile("Name : {{name}}")
-console.log(template({name:'ricky'}))
+
+//console.log(template({name:'ricky'}))
+//adding and removing log
+const dataToFed = process.argv[2]
+const template = handlebars.compile(
+    "ID:{id}",
+    
+    
+    
+    <?xml version="1.0" encoding="utf-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+  xmlns="urn:enterprise.soap.sforce.com">
+  <soapenv:Body>
+     <createResponse>
+        <result>
+           <id>003D000000OY9omIAD</id>
+           <success>true</success>
+        </result>
+        <result>
+           <id>001D000000HTK3aIAH</id>
+           <success>true</success>
+        </result>
+     </createResponse>
+  </soapenv:Body>
+</soapenv:Envelope>)
+
+const xmlOutput = template()
